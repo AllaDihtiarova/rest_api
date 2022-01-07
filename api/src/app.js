287e1ac92133ff1a9b34503e056678619d3966db
AllaDihtiarova <AllaDihtiarova@gmail.com>
2022-01-07 17:28:22 +0200

    test

--- api/src/app.js
+++ api/src/app.js
@@ -1,2 +1,8 @@
-console.log('Hello, world!')
-console.log(process.env.APP_PORT)
+console.log('Hello, world!');
+console.log(process.env.APP_PORT);
+console.log('test');
+console.log('test');
+console.log('test');
+console.log('test');
+console.log('test');
+console.log('test');
